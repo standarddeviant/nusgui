e54ba1e111a827b1897592d1f781bcaec7881b2c
standarddeviant <dave.crist@gmail.com>
2026-03-30 03:41:27 -0400

    add basic comments to convenience build+install script on linux

--- auditable_zigbuild.sh
+++ auditable_zigbuild.sh
@@ -1,5 +1,9 @@
+# install tools
 cargo install cargo-auditable cargo-zigbuild
+
+# perform build
 cargo auditable zigbuild --release
 
+# copy build artifact
 # TODO: make prompt to copy binary to ~/.cargo/bin
 cp ./target/release/nusgui ~/.cargo/bin
